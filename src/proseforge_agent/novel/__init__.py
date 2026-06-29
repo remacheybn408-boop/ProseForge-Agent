02636dda6fb855f26424d0889c122bdbbd959b89
remacheybn408 <remacheybn408@gmail.com>
2026-06-29 21:31:58 +0800

    feat: add novel project manifest

---
+++ src/proseforge_agent/novel/__init__.py
@@ -0,0 +1,5 @@
+"""Novel project operations."""
+
+from .manifest import MANIFEST_NAME, NovelProjectManifest, NovelProjectStore
+
+__all__ = ["MANIFEST_NAME", "NovelProjectManifest", "NovelProjectStore"]
