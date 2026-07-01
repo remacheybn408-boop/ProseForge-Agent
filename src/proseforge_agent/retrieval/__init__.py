8a745bba54ad330b749f0bda19006e9f1d1ea5dd
remacheybn408 <remacheybn408@gmail.com>
2026-07-01 09:47:15 +0800

    feat: add embeddings abstraction

--- src/proseforge_agent/retrieval/__init__.py
+++ src/proseforge_agent/retrieval/__init__.py
@@ -6,15 +6,20 @@ read helpers; it does not import workflow or provider implementations.
 """
 
 from .evidence import EvidencePack, EvidencePackBuilder
+from .embeddings import EmbeddingConfig, EmbeddingProvider, FakeEmbeddingProvider, build_embedding_provider
 from .index import MemoryIndex, ScoredItem
 from .router import EvidenceItem, RetrievalRequest, RetrievalRouter
 
 __all__ = [
     "EvidencePack",
     "EvidencePackBuilder",
+    "EmbeddingConfig",
+    "EmbeddingProvider",
+    "FakeEmbeddingProvider",
     "MemoryIndex",
     "ScoredItem",
     "EvidenceItem",
     "RetrievalRequest",
     "RetrievalRouter",
+    "build_embedding_provider",
 ]
